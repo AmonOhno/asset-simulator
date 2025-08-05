29eeef468a09dc946edd77e811987d48a42a60a0
AmonOhno <asiangate00815@icloud.com>
2025-08-05 23:14:39 +0900

    develop_不要ディレクトリを削除

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/会計＆資産シミュレーター/i);
-  expect(headerElement).toBeInTheDocument();
-});
