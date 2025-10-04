5e439864d65424f550f89c41b4e9964d0a2ec197
AmonOhno <asiangate00815@icloud.com>
2025-10-04 10:15:53 +0900

    reactバージョン互換性修正必要あり

--- web/src/supabaseClient.ts
+++ web/src/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://dfqtsogrhkrayfixnbtz.supabase.co';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+// const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmcXRzb2dyaGtyYXlmaXhuYnR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MzExNjIsImV4cCI6MjA2ODUwNzE2Mn0.n7y1XSWiTwrTyOibOVCfQXqP97zAAmP7u9blhqJohdY';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and Anon Key must be provided in environment variables.');
