b1e5631f5520cee06012d05db29f70792bdd59a1
ohno_a <ohno_a@worksap.co.jp>
2025-10-23 20:54:23 +0900

    使われないuseEffectを削除

--- web/src/components/event/EventScheduleManager.tsx
+++ web/src/components/event/EventScheduleManager.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useEventsStore, ScheduleEvent } from '@asset-simulator/shared';
 import { EventScheduleForm } from './EventScheduleForm';
 
