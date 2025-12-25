767f24830c7dab5fc72d7c4b412b1681b5dd066e
AmonOhno <asiangate00815@icloud.com>
2025-12-25 18:03:39 +0900

    割と大幅リファクタリング

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Aggregate styles (single import) for simpler maintenance
+import 'styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
