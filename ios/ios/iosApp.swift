5bbc95012674ca4eefd0396e9131af61aba123eb
AmonOhno <asiangate00815@icloud.com>
2026-02-11 12:29:48 +0900

    ios用アプリ作成開始

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Amon on 2026/01/21.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
