71fc8b6e84947f35c1c69a074c48ddbb0fe4c69e
AmonOhno <asiangate00815@icloud.com>
2026-06-15 23:25:59 +0900

    ender.comの料金体制変更に伴い、serverとパイプラインを削除

--- server/src/config/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { supabaseUrl, supabaseKey } from './environment'; // 環境変数を先に読み込む
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase };
